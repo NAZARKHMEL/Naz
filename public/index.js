9ad8ceb72efcecc9200ed3958c167c467293ba52
Nazar Khmel <nazkhmel@gmail.com>
2024-02-10 13:05:12 +0200

    Add existing project files to Git

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import * as path from 'path';
-import routes from './routes/routes.js';
-import adminroutes from './routes/adminroutes.js';
-import profileroutes from './routes/profileroutes.js';
-import stocksroutes from './routes/stocksroutes.js';
+import routes from '../routes/routes.js';
+import adminroutes from '../routes/adminroutes.js';
+import profileroutes from '../routes/profileroutes.js';
+import stocksroutes from '../routes/stocksroutes.js';
 import * as engines from 'consolidate';
 import config from './db.config.js'
 import { fileURLToPath } from 'url';
@@ -17,13 +17,13 @@ const __dirname = dirname(__filename);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '../views');
 app.engine('html', engines.mustache);
 app.set('view engine', 'html');
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-app.use(express.static(path.join(__dirname, 'styles')));
+app.use(express.static(path.join(__dirname, '../styles')));
 
 app.use("/prodimages", express.static('prodimages'));
 app.use("/images", express.static('images'));
